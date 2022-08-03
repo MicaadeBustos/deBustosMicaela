57b6235749edc7d3ac5d6b7fc46051743941135e
Micaela de Bustos <micaadebustos@gmail.com>
2022-08-03 04:12:03 -0300

    rehacer header

--- deBustos.Micaela/src/app/app.module.ts
+++ deBustos.Micaela/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    LogoapComponent
   ],
   imports: [
     BrowserModule,
